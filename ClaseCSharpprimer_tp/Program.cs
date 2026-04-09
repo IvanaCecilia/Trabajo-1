bbf30912fe507be4920b3823d8c4080370910e7d
Dario-Salvetti <salvettidario@outlook.com.ar>
2026-04-09 17:48:20 -0300

    organizacion estructura

--- ClaseCSharpprimer_tp/Program.cs
+++ ClaseCSharpprimer_tp/Program.cs
@@ -1,5 +1,19 @@
-﻿public interface ITransferible
+﻿
+// fin main
+public interface ITransferible
 {
     void Retirar();
 }
 abstract class Cuenta(){}
+
+class CuentaCorriente : Cuenta, ITransferible(){
+
+}
+
+class CajaAhorros : Cuenta, ITransferible (){
+
+}
+
+class Banco (){
+
+}
